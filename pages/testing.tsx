1053f20a684d9da5c294190d7a16cd60b0ed146f
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-09-07 13:12:17 +0200

    feat: full height div

--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,27 +1,9 @@
+import Testing from '@/components/Testing'
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
 
 const testing = () => {
-    useEffect(() =>{
-        let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty("--vh",`${vh}px`)
-    },[])
-  return (
-    <div>
-        <FullHeightDiv color="red">
-        <h1>testing2</h1>
-    </FullHeightDiv>
-    <FullHeightDiv color="blue">
-        <h1>testing3</h1>
-    </FullHeightDiv>
-    </div>
-  )
+    return <Testing/>
 }
 
 export default testing
 
-const FullHeightDiv = styled.div<{color:string}>`
-    border: 1px solid red;
-    background-color: ${({color})=>color && color};
-    height: calc(var(--vh,1vh)*100 - var(--header-height));
-`
