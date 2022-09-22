9e01f9f9d15a67e5fe546642f62c072d7dc8ad92
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-09-22 15:16:41 +0200

    feat: footer icons

--- components/Footer/FooterInital/FooterInitial.style.ts
+++ components/Footer/FooterInital/FooterInitial.style.ts
@@ -11,6 +11,16 @@ export const Text = styled(SText)`
 `;
 
 export const Flex = styled(SFlex)`
+  & > span {
+    flex: 1;
+  }
+
+  @media (${dimensions.tablet}) {
+    & > span {
+      flex: 0 1 18%;
+    }
+  }
+
   @media (${dimensions.desktop}) {
     flex: 20%;
     max-width: 40rem;
