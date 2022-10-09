cc01d58fc3fd214a660fe204248f34855783611c
Pavel Zapletal <pavel.zaplet25@gmail.com>
2022-10-09 22:34:43 +0200

    feat: new meta description

--- utils/HeadSelector.tsx
+++ utils/HeadSelector.tsx
@@ -9,7 +9,7 @@ const HeadSelector = () => {
       <meta name="keywords" content="Plavecká škola Kometa" />
       <meta
         name="description"
-        content="" //*TODO -> je potreba pridat nejaky meta description
+        content="Učíme plavat zábavnou formou dle nejnovějších metodik děti i dospělé. Výuku vedou kvalifikovaní trenéři na bazénech po celém Brně. Přidej se k nejlepším."
       />
     </Head>
   );
