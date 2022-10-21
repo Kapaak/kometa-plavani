41ab69ee4506f36a3319ee8a46d95c4dedca8ae7
kapaak <pavel.zaplet25@gmail.com>
2022-10-21 20:05:55 +0200

    feat: google analytics

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		styledComponents: true,
-	},
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  env: {
+    PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,10 +10,23 @@ import { GlobalStyles } from "../styles/GlobalStyles";
 import type { AppProps } from "next/app";
 //redux
 import { store } from "@/redux/store";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `}
+      </Script>
       <GlobalStyles />
       <Header />
       <HeadSelector />
