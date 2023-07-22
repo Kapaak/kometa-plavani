8d7824348acaeb9d9706ae3a5211669de452ac1f
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-07-22 18:25:05 +0200

    feat: update images

--- screens/PrihlaskyScreen/components/LecturesSection/LecturesSection.data.ts
+++ screens/PrihlaskyScreen/components/LecturesSection/LecturesSection.data.ts
@@ -2,6 +2,8 @@
 import KindergardenService from "@/public/images/service-kindergarden.jpg";
 import SchoolService from "@/public/images/service-school.jpg";
 import CourseService from "@/public/images/service-course.jpg";
+import BasicService from "@/public/images/service-basic.jpg";
+import ConditionService from "@/public/images/service-condition.jpg";
 //interfaces
 import { Service, ScrollSections, WeekDays, LectureTypes } from "@/domains";
 
@@ -25,7 +27,7 @@ export const data: Service[] = [
     headline: "Plavání pro školy",
     text: ` Cílem naší výuky je adaptovat dítě na vodní prostředí. To konkrétně zahrnuje odstranění strachu z vody, potápění, splývání, rozvoj pocitu vody a skok do vody. Poté se děti učí základy jednotlivých plaveckých způsobů. Naše metodika odpovídá směrnicím MŠMT. Výuka se však přizpůsobuje aktuálním potřebám jednotlivců, což znamená, že děti vždy získají nové dovednosti. Zároveň klademe velký důraz na to, aby děti výuka bavila. Vhodné pro děti od 4 let.
       `,
-    image: KindergardenService,
+    image: SchoolService,
     alt: "dítě v bazénu pod vodou s brýlemi",
     name: ScrollSections.kindergardens, //TODO uprav
     lectureType: LectureTypes.SCHOOL,
@@ -39,7 +41,7 @@ export const data: Service[] = [
     text: `
     Kurzy jsou určené pro děti, které se potřebují naučit plavat nebo se v plavání zdokonalit. Našim cílem je děti adaptovat na vodní prostředí. Navíc náš klub disponuje nově zpracovanou metodikou, díky které se děti naučí plavat rychle a zábavnou formou. Nejdůležitější pro nás je, aby děti měly z plavání radost.
       `,
-    image: SchoolService,
+    image: BasicService,
     alt: "plavec s hlavou pod vodou a deskou",
     name: ScrollSections.kindergardens, //TODO uprav
     lectureType: LectureTypes.BASIC,
@@ -65,7 +67,7 @@ export const data: Service[] = [
     id: 5,
     headline: "Kondiční plavání",
     text: `Kurzy jsou určeny pro děti, co se nebojí plavat ve velkém bazénu, navíc již ovládají základy plavání a v plavání se chtějí zdokonalit. Pokud již plavat umí nabízíme možnost kondičního plavání, kde děti mohou zlepšit svou fyzickou kondici a věnovat se plavání ikdyž zatím neuvažují o kariéře závodního plavce.`,
-    image: CourseService,
+    image: ConditionService,
     alt: "dvě plavkyně na začátku dráhy čekající na start",
     name: ScrollSections.kindergardens, //TODO uprav
     lectureType: LectureTypes.CONDITION,
