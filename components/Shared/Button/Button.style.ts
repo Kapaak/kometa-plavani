c8a59353b295a63cc72f73782aa5006cc4a830ac
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-07-22 18:40:06 +0200

    feat: fix issue

--- components/Shared/Button/Button.style.ts
+++ components/Shared/Button/Button.style.ts
@@ -16,7 +16,7 @@ export const Button = styled.button<ButtonProps>`
   text-transform: uppercase;
   font-family: var(--font1);
   color: var(--col2);
-  padding: 1rem 2rem;
+  padding: 1.5rem 3rem;
   cursor: pointer;
   border-radius: 3rem;
   font-size: var(--fbutton);
@@ -28,6 +28,7 @@ export const Button = styled.button<ButtonProps>`
     switch (variant) {
       case "bordered":
         return css`
+          padding: 1.5rem 3rem;
           border: 2px solid var(--col2);
           background-color: transparent;
 
@@ -42,13 +43,13 @@ export const Button = styled.button<ButtonProps>`
           return css`
             background-color: var(--col1);
             color: var(--col2);
-            padding: 1.9rem 4rem;
+            min-height: 5rem;
+
             &:hover {
-              padding: 1.9rem 5rem;
+              padding-inline: 4rem;
             }
           `;
         return css`
-          padding: 1.5rem 3rem;
           line-height: 1.6;
           background-color: var(--col2);
           color: var(--colw);
