37dce481d1b479888f5b5c9192da58a797699142
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-08-29 22:45:47 +0200

    feat: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 //libraries
 import { Provider } from "react-redux";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Analytics } from "@vercel/analytics/react";
 //components
 import { HeadSelector } from "@/shared";
 import Header from "@/components/Header/Header";
@@ -15,6 +16,7 @@ import { store } from "@/redux/store";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Analytics />
       <GoogleAnalytics
         trackPageViews
         gaMeasurementId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
