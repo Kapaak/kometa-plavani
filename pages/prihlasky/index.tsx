8f4b7da81327c3135cb6094ac3bbbc856d5a9fdf
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-09-01 09:51:42 +0200

    feat: up

--- pages/prihlasky/index.tsx
+++ pages/prihlasky/index.tsx
@@ -3,6 +3,7 @@ import { PrihlaskyScreen } from "@/screens";
 import { LecturesContextProvider } from "@/contexts";
 
 const PrihlaskyPage: NextPage = () => {
+  //
   return (
     <LecturesContextProvider>
       <main>
