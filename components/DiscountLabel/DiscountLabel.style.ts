9f24d47e902ec3c33f694bdaf1a2ae20acd09b44
Pavel Zapletal <pavel.zaplet25@gmail.com>
2023-10-23 13:35:02 +0200

    feat: change info bar styles

--- components/DiscountLabel/DiscountLabel.style.ts
+++ components/DiscountLabel/DiscountLabel.style.ts
@@ -8,7 +8,7 @@ export const DiscountLabel = styled.div`
   top: 0rem;
   left: 0;
   width: 100%;
-  padding: 0.4rem var(--side-padding);
+  padding: 1rem var(--side-padding);
   background-color: var(--col1);
   z-index: 10;
 
@@ -20,8 +20,9 @@ export const DiscountLabel = styled.div`
 export const BiggerText = styled.span`
   font-family: var(--font1);
   font-size: 2rem;
+  letter-spacing: 0.2rem;
 `;
 
 export const SmallerText = styled.span`
-  font-size: 1.4rem;
+  font-size: 1.6rem;
 `;
