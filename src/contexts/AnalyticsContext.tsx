c83cc2f37ad31f6ee5217e1ec3c82321ab95a8e3
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-07-07 22:15:15 +0200

    fix: posthog cookie

--- src/contexts/AnalyticsContext.tsx
+++ src/contexts/AnalyticsContext.tsx
@@ -8,20 +8,20 @@ import posthog from "posthog-js";
 export const AnalyticsProvider = ({ children }: PropsWithChildren) => {
   const router = useRouter();
 
-  useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      process.env.NODE_ENV === "production"
-    ) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
-        api_host:
-          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
-        loaded: (posthog) => {
-          if (process.env.NODE_ENV === "development") posthog.debug();
-        },
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (
+  //     typeof window !== "undefined" &&
+  //     process.env.NODE_ENV === "production"
+  //   ) {
+  //     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+  //       api_host:
+  //         process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+  //       loaded: (posthog) => {
+  //         if (process.env.NODE_ENV === "development") posthog.debug();
+  //       },
+  //     });
+  //   }
+  // }, []);
 
   useEffect(() => {
     // Track page views
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 import { CookieConsent } from "~/components/CookieConsent";
 import { HeadSelector } from "~/components/Shared";
@@ -13,26 +15,39 @@ import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
+//disable posthog in development
+if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AnalyticsProvider>
-        <PageContextProvider>
-          <ToastContainer
-            position="bottom-right"
-            autoClose={false}
-            closeOnClick
-            pauseOnFocusLoss
-            draggable
-            theme="colored"
-          />
+    <PostHogProvider client={posthog}>
+      <QueryClientProvider client={queryClient}>
+        <AnalyticsProvider>
+          <PageContextProvider>
+            <ToastContainer
+              position="bottom-right"
+              autoClose={false}
+              closeOnClick
+              pauseOnFocusLoss
+              draggable
+              theme="colored"
+            />
 
-          <GlobalStyles />
-          <HeadSelector />
-          <Component {...pageProps} />
-          <CookieConsent />
-        </PageContextProvider>
-      </AnalyticsProvider>
-    </QueryClientProvider>
+            <GlobalStyles />
+            <HeadSelector />
+            <Component {...pageProps} />
+            <CookieConsent />
+          </PageContextProvider>
+        </AnalyticsProvider>
+      </QueryClientProvider>
+    </PostHogProvider>
   );
 }
