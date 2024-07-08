bce5b3f69f14b7ee14121471abb61d398dee65a5
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-07-08 15:22:29 +0200

    fix: remove posthog from app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
 
 import { CookieConsent } from "~/components/CookieConsent";
 import { HeadSelector } from "~/components/Shared";
@@ -15,39 +13,26 @@ import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
-//disable posthog in development
-if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
-    // Enable debug mode in development
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug();
-    },
-  });
-}
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PostHogProvider client={posthog}>
-      <QueryClientProvider client={queryClient}>
-        <AnalyticsProvider>
-          <PageContextProvider>
-            <ToastContainer
-              position="bottom-right"
-              autoClose={false}
-              closeOnClick
-              pauseOnFocusLoss
-              draggable
-              theme="colored"
-            />
+    <QueryClientProvider client={queryClient}>
+      <AnalyticsProvider>
+        <PageContextProvider>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={false}
+            closeOnClick
+            pauseOnFocusLoss
+            draggable
+            theme="colored"
+          />
 
-            <GlobalStyles />
-            <HeadSelector />
-            <Component {...pageProps} />
-            <CookieConsent />
-          </PageContextProvider>
-        </AnalyticsProvider>
-      </QueryClientProvider>
-    </PostHogProvider>
+          <GlobalStyles />
+          <HeadSelector />
+          <Component {...pageProps} />
+          <CookieConsent />
+        </PageContextProvider>
+      </AnalyticsProvider>
+    </QueryClientProvider>
   );
 }
